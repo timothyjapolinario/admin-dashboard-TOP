6b22f1a2217c203632a068cdafff15dc4b25f029
JayJay <timothyjapolinario@gmail.com>
2022-06-23 11:34:39 +0800

    Add delete functionality of project

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
-let favorite_buttons = document.querySelectorAll(".project-options>:nth-child(1)")
 
+let projects = document.querySelectorAll(".project");
 
-favorite_buttons.forEach(button=>{
-    button.addEventListener("click", function(){
-        change_heart_style(button);
+projects.forEach(project=>{
+    //heart button
+    let heart_button = project.childNodes[5].childNodes[1]
+    let delete_button = project.childNodes[5].childNodes[5]
+    console.log(project.childNodes[5].childNodes);
+
+    heart_button.addEventListener("click", function(){
+        change_heart_style(heart_button);
     })
+    delete_button.addEventListener("click", function(){
+        delete_project(project);
+    })
+    
 })
+
 function change_heart_style(button){
     if(button.getAttribute("src") == "./images/heart-outline.svg"){
         button.src="./images/heart.svg"
     }else{
         button.src="./images/heart-outline.svg"
     }
+}
+
+function delete_project(project){
+    project.remove()
 }
